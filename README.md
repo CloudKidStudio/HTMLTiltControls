c0d29d8c14dd5ef08a896a0f016eae96a62e9012
Andrew Start <andrewstart@cloudkid.com>
2014-07-09 16:02:53 -0400

    Initial commit of the library.

---
+++ README.md
@@ -0,0 +1,23 @@
+HTML Tilt Controls
+==============
+
+Calculates device angle relative to the screen orientation, instead of in absolute values.
+
+##Usage##
+Check to make sure that it is supported and add an event listener:
+```js
+if(window.TiltControls.supported)
+	window.TiltControls.addEventListener(function(event))
+		{
+			//positive is tilted to the right, negative is to the left
+			var leftRightTilt = event.degX;
+			//positive is tilted towards the user, negative is away from the user
+			var upDownTilt = event.degY;
+		});
+```
+
+##License##
+
+Copyright (c) 2014 [CloudKid](http://github.com/cloudkidstudio)
+
+Released under the MIT License.
---
+++ TiltControls.js
@@ -0,0 +1,78 @@
+(function(global){
+
+	// The tilt controls object already exists
+	if ("TiltControls" in global) return;
+
+	var TiltControls = {};
+
+	var eventObject = {degX: 0, degY: 0};
+	var listeners = [];
+
+	Object.defineProperty(TiltControls, 'supported', {
+		get: function()
+		{
+			return !!(window.orientation !== undefined && window.DeviceOrientationEvent);
+		}
+	});
+
+	Object.defineProperty(TiltControls, 'addEventListener', {
+		enumerable: false,
+		writable:false, 
+		value: function(listener) {
+			if(typeof listener != "function") return;
+			if(listeners.indexOf(listener) == -1)
+			{
+				listeners.push(listener);
+				if(listeners.length == 1)
+				{
+					window.addEventListener('deviceorientation', eventListenerFunction);
+				}
+			}
+		}
+	});
+
+	Object.defineProperty(TiltControls, 'removeEventListener', {
+		enumerable: false,
+		writable:false, 
+		value: function(listener) {
+			var index = listeners.indexOf(listener);
+			if(index > -1)
+			{
+				listeners.splice(index, 0);
+				if(listeners.length < 1)
+					window.removeEventListener('deviceorientation', eventListenerFunction);
+			}
+		}
+	});
+
+	var eventListenerFunction = function(event)
+	{
+		switch(window.orientation)
+		{
+			case 0:
+				eventObject.degX = event.gamma;
+				eventObject.degY = event.beta;
+				break;
+			case 90:
+				eventObject.degX = event.beta;
+				eventObject.degY = -event.gamma;
+				break;
+			case -90:
+				eventObject.degX = -event.beta;
+				eventObject.degY = event.gamma;
+				break;
+			case 180:
+				eventObject.degX = -event.gamma;
+				eventObject.degY = -event.beta;
+				break;
+		}
+		for(var i = listeners.length - 1; i >= 0; --i)
+		{
+			listeners[i](eventObject);
+		}
+	};
+
+	// Assign to the global namespace
+	global.TiltControls = TiltControls;
+	
+}(window));
